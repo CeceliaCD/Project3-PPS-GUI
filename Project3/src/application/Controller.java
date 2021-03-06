633f67f5ef8d9526c4eead846e9c261deebdff3f
ansanida <33883725+ansanida@users.noreply.github.com>
2021-03-05 21:32:03 -0500

    started working on addEmployee

--- Project3/src/application/Controller.java
+++ Project3/src/application/Controller.java
@@ -3,6 +3,8 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker; // added this
+import javafx.scene.control.RadioButton; // added this 
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -23,6 +25,9 @@ public class Controller {
 
     @FXML
     private ToggleGroup dept;
+    
+    @FXML
+    private DatePicker datePicked; // added this
 
     @FXML
     private TextField annualSalary;
@@ -50,12 +55,29 @@ public class Controller {
 
     @FXML
     private Button clearButton;
+    
+    Company company = new Company(); // keep this here?
 
     @FXML
     void addEmployee(ActionEvent event) {
     	try {
     		String newEmployee = name.getText();
-   
+            RadioButton selectedButton = (RadioButton) dept.getSelectedToggle();
+    		String newDept = selectedButton.getText();
+            // need to figure out how to get date
+            Profile prof = new Profile();
+    		prof.setName(newEmployee);
+    		prof.setDept(newDept);
+    		//prof.setDateHired(date);
+            
+            Parttime parttimer = new Parttime(prof, 0, 45, 0); // making a parttime emp just to check if it was working, will add the other types soon
+    		
+    		if (company.add(parttimer) == true) {
+    			messageArea.setText("Employee added.");
+    		}
+    		else {
+    			messageArea.setText("Employee is already on the list.");
+    		}   
     	}
     	catch(NullPointerException e) {
     		throw e;
