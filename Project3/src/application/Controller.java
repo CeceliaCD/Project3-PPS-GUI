acbb695e76c5a7da7c41490e6a475a36a46f5647
ansanida <33883725+ansanida@users.noreply.github.com>
2021-03-08 01:42:22 -0500

    Update Controller.java

--- Project3/src/application/Controller.java
+++ Project3/src/application/Controller.java
@@ -62,22 +62,44 @@ public class Controller {
     void addEmployee(ActionEvent event) {
     	try {
     		String newEmployee = name.getText();
-            RadioButton selectedButton = (RadioButton) dept.getSelectedToggle();
-    		String newDept = selectedButton.getText();
-            // need to figure out how to get date
-            Profile prof = new Profile();
+    		RadioButton selectedDeptButton = (RadioButton) dept.getSelectedToggle();
+    		String newDept = selectedDeptButton.getText(); 
+    		//Date date = new Date("2/25/2012");//new Date((String) datePicked); // can't do dateHired yet bc 
+    		Date date = new Date(datePicked.getValue().toString());
+    		
+    		//messageArea.setText(newDept);
+    		Profile prof = new Profile();
     		prof.setName(newEmployee);
     		prof.setDept(newDept);
-    		//prof.setDateHired(date);
-            
-            Parttime parttimer = new Parttime(prof, 0, 45, 0); // making a parttime emp just to check if it was working, will add the other types soon
+    		prof.setDateHired(date);
     		
-    		if (company.add(parttimer) == true) {
-    			messageArea.setText("Employee added.");
+    		RadioButton selectedEmpType = (RadioButton) empType.getSelectedToggle();
+    		String newEmpType = selectedEmpType.getText();
+    		
+    		if (newEmpType.equals("Parttime")) {
+    			//int wage = Integer.parseInt(hourlyRate.getText());
+    			Parttime parttimer = new Parttime(prof, 0, 0, 0); // get hourly rate!!!
+    			parttimer.setHourlyRate(Integer.parseInt(hourlyRate.getText())); // causes an error if left blank
+    			if (company.add(parttimer) == true) {
+    				messageArea.setText("Employee added." + parttimer.toString()); // remove toString(), using it rn just to check
+    			}
+    			else {
+    				messageArea.setText("Employee is already on the list.");
+    			}
     		}
-    		else {
-    			messageArea.setText("Employee is already on the list.");
-    		}   
+    		
+    		if (newEmpType.equals("Fulltime")) {
+    			Fulltime fulltimer = new Fulltime(prof, 0, 0); // get salary!!!
+    			fulltimer.setAnnualSalary(Integer.parseInt(annualSalary.getText()));
+    			if (company.add(fulltimer) == true) {
+    				messageArea.setText("Employee added." + fulltimer.toString()); // remove toString(), using it rn just to check
+    			}
+    			else {
+    				messageArea.setText("Employee is already on the list.");
+    			}
+    		}
+    		// need to add management + check if everything is valid
+   
     	}
     	catch(NullPointerException e) {
     		throw e;
@@ -104,7 +126,14 @@ public class Controller {
 
     @FXML
     void checkHourlyRate(ActionEvent event) {
-
+        try { // doesn't work, need to fix
+    		if (Integer.parseInt(hourlyRate.getText()) < 0) {
+    			messageArea.setText("Pay rate cannot be negative.");
+    		}
+    	}
+    	catch(NumberFormatException e) {
+    		throw e;
+    	}
     }
 
     @FXML
@@ -119,7 +148,16 @@ public class Controller {
 
     @FXML
     void removeEmployee(ActionEvent event) {
-
+        // need to add something for empty employee database
+    	String newEmployee = name.getText();
+		RadioButton selectedDeptButton = (RadioButton) dept.getSelectedToggle();
+		String newDept = selectedDeptButton.getText(); 
+		Date date = new Date(datePicked.getValue().toString()); // figure out date
+		
+    	Profile prof = new Profile();
+		prof.setName(newEmployee);
+		prof.setDept(newDept);
+		prof.setDateHired(date);
     }
 
     @FXML
