22128fed95f79a23245e8067786d0d0c296cdeee
ansanida <33883725+ansanida@users.noreply.github.com>
2021-03-12 02:28:19 -0500

    Update Controller.java

--- Project3/src/application/Controller.java
+++ Project3/src/application/Controller.java
@@ -273,8 +273,8 @@ public class Controller {
     @FXML
     void printAllEmployees(ActionEvent event) {
     	if(numEmp > 0) {
-    		messageAreaDB.appendText("--Printing earning statements for all employees--");
-        	company.print();
+    		messageAreaDB.appendText("--Printing earning statements for all employees--\n");
+        	messageAreaDB.appendText(company.print() + "\n"); // nida: did this bc i couldn't see the ouput, print() would have to return a string
     	}else {
     		messageAreaDB.appendText("Employee database is empty.\n");
     	}
@@ -284,7 +284,7 @@ public class Controller {
     void printByDate(ActionEvent event) {
     	if(numEmp > 0) {
     		messageAreaDB.appendText("--Printing earning statements by date hired--");
-        	company.printByDate();
+        	company.printByDate(); // messageAreaDB.appendText(company.printByDate() + "\n");
     	}else {
     		messageAreaDB.appendText("Employee database is empty.\n");
     	}
@@ -294,7 +294,7 @@ public class Controller {
     void printByDept(ActionEvent event) {
     	if(numEmp > 0) {
     		messageAreaDB.appendText("--Printing earning statements by department--");
-        	company.printByDepartment();
+        	company.printByDepartment(); // messageAreaDB.appendText(company.printByDepartment() + "\n");
     	}else {
     		messageAreaDB.appendText("Employee database is empty.\n");
     	}
